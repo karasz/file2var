607732df1771fc4587d37967f298fb041cca7f56
Alejandro Mery <amery@geeks.cl>
2017-11-30 23:45:53 +0000

    file2govar: add expanding methods
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- cmd/file2govar/file2govar.go
+++ cmd/file2govar/file2govar.go
@@ -57,6 +57,7 @@ func main() {
 		}
 
 		fmt.Fprintf(f, "package %s\n\n//go:generate file2govar -p %s %s\n\n", *pkgname, *pkgname, fname)
+		fmt.Fprintf(f, "import (\n\t\"bytes\"\n\t\"io\"\n)\n\n")
 		fmt.Fprintf(f, "const z_%s = []byte{\n", varname)
 		for i, b := range buf.Bytes() {
 			var pre, post string
@@ -73,7 +74,14 @@ func main() {
 			}
 			fmt.Fprintf(f, "%s0x%02x%s", pre, b, post)
 		}
-		fmt.Fprintf(f, "}\n")
+		fmt.Fprintf(f, "}\n\n")
+		fmt.Fprintf(f, "func %s() ([]byte, error) {\n", varname)
+		fmt.Fprintf(f, "\tvar in = bytes.NewReader(z_%s)\n", varname)
+		fmt.Fprintf(f, "\tvar out bytes.Buffer\n")
+		fmt.Fprintf(f, "\tz, err := zlib.NewReader(&in)\n")
+		fmt.Fprintf(f, "\tif err != nil {\n\t\treturn nil, err\n\t}\n")
+		fmt.Fprintf(f, "\tio.Copy(&out, z)\n")
+		fmt.Fprintf(f, "\treturn out.Bytes(), nil\n}\n")
 		f.Close()
 	}
 }
